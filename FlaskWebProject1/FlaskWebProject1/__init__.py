8682831d803ec5f220d7778d0581622decd00384
Lincher <lcjive@gmail.com>
2017-06-26 22:43:06 +0800

    使用蓝图 路由模块化

--- FlaskWebProject1/FlaskWebProject1/__init__.py
+++ FlaskWebProject1/FlaskWebProject1/__init__.py
@@ -1,7 +1,7 @@
 """
 The flask application package.
 """
-from flask import Flask
+from flask import Flask, blueprints
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (LoginManager, login_fresh,
 login_required, login_url, login_user, logout_user, 
@@ -12,5 +12,8 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app=app)
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+
 
 from FlaskWebProject1 import views, models
---
+++ FlaskWebProject1/FlaskWebProject1/views_auth.py
@@ -0,0 +1,42 @@
+import json
+from datetime import datetime
+from flask import (render_template, request, redirect, url_for, session, flash,
+g, blueprints)
+from FlaskWebProject1 import (app, models, login_manager)
+from .models import *
+
+# url redirect
+# 蓝图，路由模块化
+auth = blueprints('auth',__name__)
+app.register_blueprint(auth, url_prefix='/auth')
+
+# 手工版login
+# @app.route('/login', methods=['POST'])
+# def login():
+#     user = models.User.query.filter_by(
+#         email=request.form.get('email')).first()
+#     #  不加first()或者all()只是一个查询对象，并没有执行
+#     #  JSON 只能用双引号
+#     if user is None:
+#         data = {"loginSuccess": False,
+#                 "loginFailedReason": "该邮箱不存在"}
+#         return json.dumps(data, ensure_ascii=False)
+#     else:
+#         if user.password == request.form.get("password"):
+#             return json.dumps({"loginSuccess":True},
+#             ensure_ascii=False)
+#         else:
+#             return json.dumps({"loginSuccess": False,
+#                                "loginFailedReason": "密码错误"}, ensure_ascii=False)
+
+
+
+# flask-login版login
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    form = 
+                            
+                        
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
