da89abf838802968a85766501df70a57f1785688
linchuanjie <814624313@qq.com>
2017-08-30 17:21:58 +0800

    update

--- FlaskWebProject1/models.py
+++ FlaskWebProject1/models.py
@@ -1,44 +1,3 @@
-<<<<<<< HEAD:FlaskWebProject1/FlaskWebProject1/models.py
-import datetime
-from FlaskWebProject1 import db, UserMixin
-from sqlalchemy import types
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(12))
-    permission = db.Column(db.String(12),default="guest")
-    gander = db.Column(db.Boolean)
-    birthdate = db.Column(db.DateTime)
-    introduction = db.Column(types.Text,default="这个人很懒，没有任何说明")
-    avatar = db.Column(types.String(),default="user.png")
-    post = db.relationship('Post',backref='author',lazy='dynamic')
-
-    def __init__(self,**kw):
-        super() .__init__(**kw)
-
-
-    def __repr__(self):
-        return '<User %r>,email:%r,password:%r,permission:%r,\
-        gander:%r,birthdata:%r,avatar:%r' % (self.username,self.email,
-        self.password,self.permission,self.gander,self.birthdate,
-        self.avatar    )
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20))
-    # 标题不能为空
-    body = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
-
-    def __init__(self,**kw):
-        super().__init__(**kw)
-        self.timestamp = datetime.datetime.utcnow()
-
-    def __repr__(self):
-=======
 import datetime
 from FlaskWebProject1 import db, UserMixin
 from sqlalchemy import types
@@ -80,5 +39,4 @@ class Post(db.Model):
         self.timestamp = datetime.datetime.utcnow()
 
     def __repr__(self):
->>>>>>> backup:FlaskWebProject1/models.py
         return "Title: %s"%self.title
