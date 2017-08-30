2a2a402da0bba61ddecfe3fc2d4714c60f28f2f0
linchuanjie <814624313@qq.com>
2017-08-30 17:16:31 +0800

    merge ZZ

--- FlaskWebProject1/__init__.py
+++ FlaskWebProject1/__init__.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD:FlaskWebProject1/FlaskWebProject1/__init__.py
-"""
-The flask application package.
-"""
-from flask import (Flask, render_template, request, redirect, url_for, session,
- flash,g, Blueprint,send_from_directory,current_app)
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import (LoginManager, login_fresh,
-login_required, login_url, login_user, logout_user, 
-UserMixin,current_user)
-from werkzeug import secure_filename
-from .config import *
-
-app = Flask(__name__)
-
-app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
-login_manager = LoginManager(app=app)
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
-
-
-from FlaskWebProject1 import views, models, views_auth
-=======
 """
 The flask application package.
 """
@@ -44,6 +20,4 @@ login_manager = LoginManager(app=app)
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-
 from FlaskWebProject1 import views, models, views_auth
->>>>>>> backup:FlaskWebProject1/__init__.py
